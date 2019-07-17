34201a85f186a88da72f50161b0748ad829278ac
Cedric M'PASSI <cempassi@student.42.fr>
2019-07-17 20:56:57 +0200

    [UPDATE] zsh-syntax-highlighting

--- setup_config.sh
+++ setup_config.sh
@@ -3,3 +3,5 @@
 ln -sf ~/.config/.zshrc ~/.zshrc
 ln -sf ~/.config/.vimrc ~/.vimrc
 ln -sf ~/.config/personal.zsh-theme ~/.oh-my-zsh/custom/themes/personal.zsh-theme
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+source ~/.zshrc
