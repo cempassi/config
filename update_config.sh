1bb9626dbfd79dafd97fa69f8ab6221b466bbce4
Cedric M'Passi <cedricmpassi@gmail.com>
2020-07-27 05:17:41 +0200

    update config script update

--- update_config.sh
+++ update_config.sh
@@ -6,13 +6,13 @@
 #    By: cempassi <cempassi@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/02/21 17:21:13 by cempassi          #+#    #+#              #
-#    Updated: 2019/03/29 04:03:01 by cempassi         ###   ########.fr        #
+#    Updated: 2020/07/27 05:17:24 by cempassi         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 #!/bin/sh
 
-cd ~/.config
+cd ~/config
 git commit -am "dotfiles update"
 git push
 cd -
