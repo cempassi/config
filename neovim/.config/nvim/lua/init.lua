9b027950f7dc233748bbeaeec31de7dc4305a166
Cedric M'Passi <cempassi@student.42.fr>
2021-01-13 22:10:14 +0100

    Update init.lua
    Dap support

--- neovim/.config/nvim/lua/init.lua
+++ neovim/.config/nvim/lua/init.lua
@@ -1,3 +1,5 @@
+require('telescope_local')
+
 local api = vim.api
 
 -- CHADTree settings
@@ -13,57 +15,67 @@ api.nvim_set_var("chadtree_settings",{
 		}
 	}
 )
+-- nvim-dap
+--
+local dap = require 'dap'
 
--- Telescope settings
-local actions = require('telescope.actions')
-
-require('telescope').setup {
-  defaults = {
-    mappings = {
-	i = {
-      ["<C-n>"] = actions.move_selection_next,
-      ["<C-p>"] = actions.move_selection_previous,
-
-      ["<leader>q"] = actions.close,
-
-      ["<C-j>"] = actions.move_selection_next,
-      ["<C-k>"] = actions.move_selection_previous,
-
-      ["<CR>"] = actions.goto_file_selection_edit + actions.center,
-      ["<leader>hs"] = actions.goto_file_selection_split,
-      ["<leader>vs"] = actions.goto_file_selection_vsplit,
-      ["<leader>te"] = actions.goto_file_selection_tabedit,
-
-      ["<C-u>"] = actions.preview_scrolling_up,
-      ["<C-d>"] = actions.preview_scrolling_down,
-
-    },
-
-    n = {
-      ["jk"] = actions.close,
-      ["<leader>q"] = actions.close,
-      ["<CR>"] = actions.goto_file_selection_edit + actions.center,
-      ["<C-x>"] = actions.goto_file_selection_split,
-      ["<C-v>"] = actions.goto_file_selection_vsplit,
-      ["<C-t>"] = actions.goto_file_selection_tabedit,
-
-      ["j"] = actions.move_selection_next,
-      ["k"] = actions.move_selection_previous,
-
-      ["<C-u>"] = actions.preview_scrolling_up,
-      ["<C-d>"] = actions.preview_scrolling_down,
-      }
+dap.adapters.c = {
+      type = 'executable',
+      attach = {
+        pidProperty = "pid",
+        pidSelect = "ask"
+      },
+      command = 'lldb-vscode', -- my binary was called 'lldb-vscode-11'
+      env = {
+        LLDB_LAUNCH_FLAG_LAUNCH_IN_TTY = "YES"
+      },
+      name = "lldb"
     }
-  }
-}
+
+dap.adapters.rust = {
+      type = 'executable',
+      attach = {
+        pidProperty = "pid",
+        pidSelect = "ask"
+      },
+      command = 'lldb-vscode', -- my binary was called 'lldb-vscode-11'
+      env = {
+        LLDB_LAUNCH_FLAG_LAUNCH_IN_TTY = "YES"
+      },
+      name = "lldb"
+    }
+
+dap.configurations.c = {
+      {
+        type = 'c';
+        request = 'launch';
+        name = "ft_ping";
+        program = "./ft_pingdb";
+        args = {"google.fr"}
+      },
+    }
+
+dap.configurations.rust = {
+      {
+        type = 'c';
+        request = 'launch';
+        name = "day05";
+        program = "./target/debug/deps/day05";
+        args = {"google.fr"}
+      },
+    }
+
+vim.cmd [[
+    command! -complete=file -nargs=* DebugC lua require "my_debug".start_c_debugger({<f-args>}, "lldb")
+]]
 
 -- Tree-sitter configuration
 require'nvim-treesitter.configs'.setup {
-		ensure_installed = {lua}, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+		ensure_installed = {maintained}, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
 		highlight = {
 			enable = true,              -- false will disable the whole extension
 			use_languagetree = false, -- Use this to enable language injection (this is very unstable)
-			disable = { "c", "rust" },  -- list of language that will be disabled
+			disable = {},  -- list of language that will be disabled
 		},
 		indent = {
 			enable = true
@@ -76,6 +88,7 @@ require'nvim-treesitter.configs'.setup {
 		}
 	}
 
+-- Corpus settings
 CorpusDirectories = {
       ['~/corpus'] = {
         autoreference = 1,
